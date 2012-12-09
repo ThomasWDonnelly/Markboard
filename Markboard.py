772537b710986315fd250d89caebbd0c86ab17cf
Daniel Shannon <d@daniel.sh>
2012-12-08 20:32:16 -0500

    Updated documentation in advance of forum announcement

--- Markboard.py
+++ Markboard.py
@@ -7,13 +7,25 @@ import sublime_plugin
 import markdown2
 import MarkboardClippers
 
+
 '''
-    OS X docs: http://www.libertypages.com/clarktech/?p=3299
-    Windows docs: http://code.activestate.com/recipes/474121-getting-html-from-the-windows-clipboard/
-    Linux docs: http://www.pygtk.org/docs/pygtk/class-gtkclipboard.html#method-gtkclipboard--store
-                http://developer.gnome.org/gtkmm/stable/classGtk_1_1Clipboard.html
-                http://stackoverflow.com/questions/1992869/how-to-paste-html-to-clipboard-with-gtk
-                http://gtk.php.net/manual/en/gtk.enum.targetflags.php
+    Grateful thanks to the following people for their work, which constitutes the bricks of this
+    little script:
+        * Trent Mick, author of python-markdown2 (https://github.com/trentm/python-markdown2),
+          which processes the Markdown;
+        * Phillip Piper, who wrote the snippet that interacts with the clipboard under Windows; and
+        * "Clark," whose guide explained the proper way to use the Foundation and AppKit libraries
+          to access OS X's clipboard.
+    I can't blame anyone but myself for the gtk scripting, which is as yet untested and likely to be
+    less than impeccable; nor for the code's mortar, which largely ditto.
+                                                    ---Daniel Shannon, d@daniel.sh, http://daniel.sh
+
+        OS X docs: http://www.libertypages.com/clarktech/?p=3299
+     Windows docs: http://code.activestate.com/recipes/474121-getting-html-from-the-windows-clipboard/
+       Linux docs: http://www.pygtk.org/docs/pygtk/class-gtkclipboard.html#method-gtkclipboard--store
+                   http://developer.gnome.org/gtkmm/stable/classGtk_1_1Clipboard.html
+                   http://stackoverflow.com/questions/1992869/how-to-paste-html-to-clipboard-with-gtk
+                   http://gtk.php.net/manual/en/gtk.enum.targetflags.php
 '''
 
 
@@ -22,10 +34,6 @@ def err(theError):
 
 
 class markboardCopyFormattedCommand(sublime_plugin.TextCommand):
-    """
-        Renders the contents of the buffer in Markdown and copies
-        the formatted text to the clipboard.
-    """
     def is_enabled(self):
         multimarkdown = self.view.score_selector(0, "text.html.multimarkdown") > 0
         markdown = self.view.score_selector(0, "text.html.markdown") > 0
@@ -40,7 +48,7 @@ class markboardCopyFormattedCommand(sublime_plugin.TextCommand):
         env['PATH'] = env['PATH'] + ":" + sublime.load_settings("Markboard.sublime-settings").get("pandoc_path", "/usr/local/bin")
 
         f = tempfile.NamedTemporaryFile(mode="w+", suffix=".mdown", delete=False)
-        self.globalWriter = f.name
+        writer = f.name
         f.close()
         self.runningThreadBuffer = ""
 
@@ -51,7 +59,7 @@ class markboardCopyFormattedCommand(sublime_plugin.TextCommand):
                 singleCursors += 1
             else:
                 normalString = self.normalize_line_endings(theSubstring)
-                f = open(self.globalWriter, "a")
+                f = open(writer, "a")
                 normalString = normalString.encode("utf-8")
                 f.write(normalString + "\n\n")
                 passedSelections += 1
@@ -60,12 +68,12 @@ class markboardCopyFormattedCommand(sublime_plugin.TextCommand):
         if singleCursors > 0 and passedSelections < 1:
             theBuffer = self.view.substr(sublime.Region(0, self.view.size()))
             normalString = self.normalize_line_endings(theBuffer)
-            f = open(self.globalWriter, "a")
+            f = open(writer, "a")
             normalString = normalString.encode("utf-8")
             f.write(normalString + "\n\n")
             f.close()
 
-        newThread = MarkboardMarkdownProcessor(self.globalWriter) if not pandoc else MarkboardPandocMarkdownProcessor(self.globalWriter, self.view.window(), env)
+        newThread = MarkboardMarkdownProcessor(writer) if not pandoc else MarkboardPandocMarkdownProcessor(self.globalWriter, self.view.window(), env)
         threads.append(newThread)
         newThread.start()
 
@@ -142,7 +150,6 @@ class MarkboardPandocMarkdownProcessor(threading.Thread):
 
     def run(self):
         f = tempfile.NamedTemporaryFile(mode="w+", suffix=".html", delete=False)
-        # f.write("\n")
         outFile = f.name
         f.close()
         cmd = ['pandoc', self.myFilename, '--output=%s' % outFile, '--from=markdown', '--to=html', '--smart', '--normalize']
